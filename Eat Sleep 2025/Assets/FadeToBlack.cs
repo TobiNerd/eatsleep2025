d9ae3698dbaef06f60710db78678467cd951fe4b
Jonas Vendelbo Hansen <joni1234@hotmail.dk>
2025-04-05 22:19:19 +0200

    Fix NRE

--- Eat Sleep 2025/Assets/FadeToBlack.cs
+++ Eat Sleep 2025/Assets/FadeToBlack.cs
@@ -20,13 +20,13 @@ public class FadeToBlack : Singleton<FadeToBlack>
         AnimationIndex index = GameManager.I.Animations.Add();
         _black.experimental.animation.Start(_black.style.opacity.value, 1.0f, fadeDurationMS, (ve, t) => ve.style.opacity = t).OnCompleted(() => GameManager.I.Animations.Complete(index));
         const float delayPercentage = 0.5f;
-        if (withBlood) _red.experimental.animation.Start(_red.style.opacity.value, 1.0f, fadeDurationMS, (ve, t) => ve.style.opacity = (t - delayPercentage) / (1 - delayPercentage)).OnCompleted(() => GameManager.I.Animations.Complete(index));
+        if (withBlood) _red.experimental.animation.Start(_red.style.opacity.value, 1.0f, fadeDurationMS, (ve, t) => ve.style.opacity = (t - delayPercentage) / (1 - delayPercentage));
     }
     public void Clear(int fadeDurationMS)
     {
         Debug.Log($"[{nameof(FadeToBlack)}] {nameof(Clear)}");
         AnimationIndex index = GameManager.I.Animations.Add();
         _black.experimental.animation.Start(_black.style.opacity.value, 0.0f, fadeDurationMS, (ve, t) => ve.style.opacity = t).OnCompleted(() => GameManager.I.Animations.Complete(index));
-        _red.experimental.animation.Start(_red.style.opacity.value, 0.0f, fadeDurationMS, (ve, t) => ve.style.opacity = t).OnCompleted(() => GameManager.I.Animations.Complete(index));
+        _red.experimental.animation.Start(_red.style.opacity.value, 0.0f, fadeDurationMS, (ve, t) => ve.style.opacity = t);
     }
 }
