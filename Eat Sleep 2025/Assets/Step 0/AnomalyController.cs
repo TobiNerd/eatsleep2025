45b2e70caab6933b9c89cef3f6f418b80ba90e19
Jonas Vendelbo Hansen <joni1234@hotmail.dk>
2025-04-05 11:11:41 +0200

    Simple anomaly test. And scene into prefabs

---
+++ Eat Sleep 2025/Assets/Step 0/AnomalyController.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace EatSleep.Step0
+{
+    public class AnomalyController : MonoBehaviour
+    {
+        [SerializeField] private List<AnomalySwap> anomalies = new();
+
+        [ContextMenu("Update Anomalies")]
+        void UpdateAnomalyList()
+        {
+            MonoBehaviour[] allBehaviours = FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.InstanceID);
+            anomalies = allBehaviours.OfType<AnomalySwap>().ToList();
+            foreach (AnomalySwap anomaly in anomalies)
+            {
+                anomaly.OriginalPosition = anomaly.transform.position;
+                anomaly.OriginalRotation = anomaly.transform.rotation;
+            }
+            Debug.Log($"Total objects: {anomalies.Count}");
+        }
+
+        [ContextMenu("Do Anomaly")]
+        bool DoAnomaly()
+        {
+            // Find two random objects swap them
+            if (anomalies.Count < 2)
+            {
+                Debug.LogWarning($"[ANOMALY] Too few anomalies, found {anomalies.Count} at least 2 needed to swap");
+                return false;
+            }
+
+            int n1 = Random.Range(0, anomalies.Count);
+            int n2 = Random.Range(0, anomalies.Count - 1);
+            if (n2 >= n1) n2++;
+
+            AnomalySwap a1 = anomalies[n1];
+            AnomalySwap a2 = anomalies[n2];
+            a1.transform.AnomalySwap(a2.transform, SwapFlags.Position | SwapFlags.Rotation);
+
+            return true;
+        }
+
+        [ContextMenu("Reset Scene")]
+        void ResetRoom()
+        {
+
+        }
+    }
+}
---
+++ Eat Sleep 2025/Assets/Step 0/AnomalySwap.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace EatSleep.Step0
+{
+    public sealed class AnomalySwap : MonoBehaviour
+    {
+        public Vector3 OriginalPosition { get; set; }
+        public Quaternion OriginalRotation { get; set; }
+    }
+}
