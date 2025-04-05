1b0a27b8e8e8c2834cf0f19d57c9fc259546e1cb
Tobi <tobinerd@hutlihut.dk>
2025-04-05 13:08:51 +0200

    basic camera controller

---
+++ Eat Sleep 2025/Assets/Scripts/InputActionReferences.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Input/InputActionReferences")]
+public class InputActionReferences : ScriptableObject
+{
+    public InputActionReference MouseDelta;
+}
