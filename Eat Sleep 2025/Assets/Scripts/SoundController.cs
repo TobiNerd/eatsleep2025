1feb631c049b3bcf9fc99afd90cc01e062465813
Jonas Vendelbo Hansen <joni1234@hotmail.dk>
2025-04-06 11:31:16 +0200

    Win game condition and bug fixes

--- Eat Sleep 2025/Assets/Scripts/SoundController.cs
+++ Eat Sleep 2025/Assets/Scripts/SoundController.cs
@@ -1,18 +1,17 @@
 using FMODUnity;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class SoundController : MonoBehaviour
 {
-    public class SoundController : MonoBehaviour
-    {
-        public StudioEventEmitter Gun;
-        public StudioEventEmitter Survived;
-        public StudioEventEmitter Win;
-        public StudioEventEmitter Lost;
-        public StudioEventEmitter LayDown;
-        public StudioEventEmitter Gasp;
-        public StudioEventEmitter CockShotgun;
-        public StudioEventEmitter Situp;
+    public StudioEventEmitter Gun;
+    public StudioEventEmitter Survived;
+    public StudioEventEmitter Win;
+    public StudioEventEmitter Lost;
+    public StudioEventEmitter LayDown;
+    public StudioEventEmitter Gasp;
+    public StudioEventEmitter CockShotgun;
+    public StudioEventEmitter Situp;
 
-    }
+
+    public Light sunWinningLight;
 }
